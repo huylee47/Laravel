5032eabf5824ebdc61919ded79ae823d3ffd1cc2
huylee47 <huyw300@gmail.com>
2024-09-25 08:58:59 +0700

    Middleware (base) & route

--- Backend/app/Models/Accounts.php
+++ Backend/app/Models/Accounts.php
@@ -7,11 +7,19 @@ use Illuminate\Database\Eloquent\Model;
 
 class Accounts extends Model
 {
-    protected $table = 'accounts';
-     protected $fillable = [
-        'account_id',
-        'username',
-        'password',
-        'role',
-     ];
+   protected $table = 'accounts';
+   protected $fillable = [
+      'account_id',
+      'username',
+      'password',
+      'role',
+   ];
+   public function isAdmin()
+   {
+      return $this->role === 1;
+   }
+   public function isUser()
+   {
+      return $this->role === 0;
+   }
 }
--- Backend/routes/web.php
+++ Backend/routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Admin\AccountController;
+use App\Http\Controllers\Admin\BlogController;
+use App\Http\Controllers\Admin\ProductController;
+use App\Models\Products;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -13,17 +16,28 @@ Route::prefix('/Admin')->group(function () {
         Route::get('/list',[AccountController::class,'indexAccount'])->name('admin.Account.index');
         Route::get('/Create',[AccountController::class,'createAccountView'])->name('admin.Account.add');
         Route::post('/CreateAcc',[AccountController::class,'createAccount'])->name('admin.Account.create');
-        Route::get('/Info/{id}',[AccountController::class,'getAccountId'])->name('admin.Account.show');
-        Route::get('/Edit/{id}',[AccountController::class,'updateAccountView'])->name('admin.Account.edit');
-        Route::post('/Edit/{id}',[AccountController::class,'updateAccount']);
-        Route::get('/Delete/{id}',[AccountController::class,'deleteAccount']);
-        Route::post('/ChangeRole/{id}',[AccountController::class,'changeRole']);
+        // Route::get('/Info/{id}',[AccountController::class,'getAccountId'])->name('admin.Account.show');
+        Route::get('/Edit/{id}',[AccountController::class,'updateAccountView'])->name('admin.Account.editView');
+        Route::post('/EditAcc/{id}',[AccountController::class,'updateAccount'])->name('admin.Account.edit');
+        Route::get('/Delete/{id}',[AccountController::class,'deleteAccount'])->name('admin.Account.delete');
     });
     Route::prefix('/Product')->group(function () {
-    
+        Route::get('/list',[ProductController::class,'indexProduct'])->name('admin.Product.index');
+        Route::get('/Create',[ProductController::class,'createProductView'])->name('admin.Product.add');
+        Route::post('/CreateProduct',[ProductController::class,'createProduct'])->name('admin.Product.create');
+        // Route::get('/Info/{id}',[ProductController::class,''])->name('admin.Product.show');
+        Route::get('/Edit/{id}',[ProductController::class,'updateProductView'])->name('admin.Product.editView');
+        Route::post('/EditProduct/{id}',[ProductController::class,'updateProduct'])->name('admin.Product.edit');
+        Route::get('/Delete/{id}',[ProductController::class,'deleteProduct'])->name('admin.Product.delete');
     });
     
     Route::prefix('/Blog')->group(function () {
-    
+        Route::get('/list',[BlogController::class,'indexBlog'])->name('admin.Blog.index');
+        Route::get('/Create',[BlogController::class,'createBlogView'])->name('admin.Blog.add');
+        Route::post('/CreateBlog',[BlogController::class,'createBlog'])->name('admin.Blog.create');
+        // Route::get('/Info/{id}',[BlogController::class,''])->name('admin.Blog.show');
+        Route::get('/Edit/{id}',[BlogController::class,'getBlogId'])->name('admin.Blog.editView');
+        Route::post('/EditBlog/{id}',[BlogController::class,'updateBlog'])->name('admin.Blog.edit');
+        Route::get('/Delete/{id}',[BlogController::class,'deleteBlog'])->name('admin.Blog.delete');
     });
 });
