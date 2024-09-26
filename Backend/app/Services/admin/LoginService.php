f52ce2811a64470bf708e62f3a183cebabb85a5f
nhat191024 <richberchannel01@gmail.com>
2024-09-26 22:16:41 +0700

    Refactor LoginService to improve code readability and maintainability

--- Backend/app/Services/admin/LoginService.php
+++ Backend/app/Services/admin/LoginService.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 namespace App\Services\admin;
 
 use App\Models\Accounts;
@@ -32,28 +32,28 @@ class LoginService {
     //     } else {
     //         Auth::logout();
     //         return response()->json(['error' => 'You are banned.'], 403);
-    //     }   
+    //     }
     // }
     public function loginAuth(Request $request) {
         $request->validate([
             'username' => 'required|string|max:255',
             'password' => 'required|string',
         ]);
-    
+
         $account = Accounts::where('username', $request->input('username'))->first();
-    
+
         if (!$account) {
             return ['success' => false, 'error' => 'Username is incorrect.'];
         }
-    
+
         if (!Hash::check($request->input('password'), $account->password)) {
             return ['success' => false, 'error' => 'Password is incorrect.'];
         }
-    
+
         // Đăng nhập và tạo token
         Auth::login($account);
-        $token = $account->createToken('authToken')->plainTextToken; // Tạo token
-    
+        $token = $account->createToken('authToken',  ['ability:admin'])->plainTextToken; // Tạo token
+
         if ($account->role == 1) {
             return ['success' => true, 'token' => $token, 'role' => 'User'];
         } elseif ($account->role == 0) {
@@ -63,6 +63,6 @@ class LoginService {
             return ['success' => false, 'error' => 'You are banned.'];
         }
     }
-    
+
 }
 ?>
