08157c02f1c6d7061f7d1086f84a090003d641cb
huylee47 <huyw300@gmail.com>
2024-09-30 09:00:25 +0700

    clean code blog ctrl

--- Backend/app/Http/Controllers/Admin/BlogController.php
+++ Backend/app/Http/Controllers/Admin/BlogController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BlogRequest;
 use App\Services\admin\Blogservice;
-use Illuminate\Http\Request;
 
 class BlogController extends Controller
 {
@@ -15,13 +14,13 @@ class BlogController extends Controller
     {
         $this->blogService = $blogService;
     }
-
+    // get all blog posts
     public function indexBlog()
     {
         $blogs = $this->blogService->getAllBlogs();
-        return response()->json($blogs);  
+        return response()->json($blogs);
     }
-
+    // get blog post by id
     public function getBlogId($id)
     {
         $blog = $this->blogService->getBlogId($id);
@@ -32,16 +31,16 @@ class BlogController extends Controller
 
         return response()->json($blog);
     }
-
+    // create new blog post
     public function createBlog(BlogRequest $request)
     {
         $blog = $this->blogService->createBlog($request);
         return response()->json([
             'status' => 'successs!',
             'newBlog' => $blog,
-        ], 201); 
+        ], 201);
     }
-
+    // update blog post by id
     public function updateBlog($id, BlogRequest $request)
     {
         $blog = $this->blogService->getBlogId($id);
@@ -57,7 +56,7 @@ class BlogController extends Controller
             'updatedBlog' => $newBlog,
         ]);
     }
-
+    // delete blog post by id
     public function deleteBlog($id)
     {
         $blog = $this->blogService->getBlogId($id);
@@ -65,12 +64,10 @@ class BlogController extends Controller
         if (!$blog) {
             return response()->json(['error' => 'Blog not found'], 404);
         }
-
         $this->blogService->deleteBlog($id);
-
         return response()->json([
             'status' => 'success!',
-            'deletedBlog' => $blog ,
+            'deletedBlog' => $blog,
         ]);
     }
 }
