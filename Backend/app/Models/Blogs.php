c2577b1130137ef847c7ec3a10fb82149dea6f63
huylee47 <huyw300@gmail.com>
2024-09-30 09:01:36 +0700

    clean code model

--- Backend/app/Models/Blogs.php
+++ Backend/app/Models/Blogs.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Blogs extends Model
 {
+    use HasFactory;
     protected $table = 'blogs';
      protected $fillable = [
         'blog_id',
--- Backend/app/Models/Products.php
+++ Backend/app/Models/Products.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
+    use HasFactory;
     protected $table = 'products';
      protected $fillable = [
         'product_id',
