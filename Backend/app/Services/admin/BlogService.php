3deea5c0f049749ed8f4a9697513408c6ad1ff84
huylee47 <huyw300@gmail.com>
2024-09-30 09:01:57 +0700

    clean code blog service

--- Backend/app/Services/admin/BlogService.php
+++ Backend/app/Services/admin/BlogService.php
@@ -5,14 +5,15 @@ use App\Http\Requests\BlogRequest;
 use App\Models\Blogs;
 
 class BlogService {
+    // Get all blog services
     public function getAllBlogs() {
         return Blogs::all();
     }
-
+    // Get blog by ID service
     public function getBlogId($id) {
         return Blogs::find($id);
     }
-
+    // Create blog service
     public function createBlog(BlogRequest $request) {
         $newBlog = Blogs::create([
             'title' => $request->title,
@@ -21,7 +22,7 @@ class BlogService {
         ]);
         return $newBlog;
     }
-
+    // Update blog service
     public function updateBlog($id, BlogRequest $request) {
         $blog = Blogs::findOrFail($id);
         $blog->update([
@@ -31,7 +32,7 @@ class BlogService {
         ]);
         return $blog;
     }
-
+    // Delete blog service 
     public function deleteBlog($id) {
         $blog = Blogs::findOrFail($id);
         $blog->delete();
